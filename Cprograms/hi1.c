ea1897624f2db241cc5397b8ae1f2e62a6d26cce
d0rara <tribunejyotisaikia09421@gmail.com>
2025-11-20 00:01:25 +0530

    while loop hi

---
+++ Cprograms/hi1.c
@@ -0,0 +1,13 @@
+// Program to print HI some number of times using while loop
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 1; // initialization
+    while (i <= 5)
+    {
+        printf("HI\n");
+        i++;
+    }
+}
