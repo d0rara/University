e41b364aede00dd49ab4489788a01a8d20929012
d0rara <tribunejyotisaikia09421@gmail.com>
2026-01-21 16:06:32 +0530

    positivenegative

---
+++ Cprograms/numbers3.c
@@ -0,0 +1,26 @@
+//  Program to find whether a number is positive or negative.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0)
+    {
+        printf("The number is positive\n");
+    }
+    else if (num < 0)
+    {
+        printf("The number is negative\n");
+    }
+    else
+    {
+        printf("The number is zero\n");
+    }
+
+    return 0;
+}
