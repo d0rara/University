8917579f58eaa2561716f40bc12e3c8be39611c2
d0rara <tribunejyotisaikia09421@gmail.com>
2026-01-21 15:19:45 +0530

    triangle

---
+++ Cprograms/triangle.c
@@ -0,0 +1,20 @@
+// Program to find the area of a right angled triangle
+
+#include <stdio.h>
+
+int main(void)
+{
+    int base, height;
+
+    // Prompt user for base
+    printf("Enter the base of the triangle: ");
+    scanf("%d", &base);
+
+    // Prompt user for height
+    printf("Enter the height of the triangle: ");
+    scanf("%d", &height);
+
+    // Area = 1/2*base*height
+    float area = 0.5*(base*height);
+    printf("The area of the right angled triangle is %.02f\n", area);
+}
