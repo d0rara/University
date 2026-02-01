01aa70137efffaeb036c9ff74ddf7503520974a7
d0rara <tribunejyotisaikia09421@gmail.com>
2026-02-02 01:09:39 +0530

    changes

---
+++ Cprograms/integer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, first, last;
+
+    // Prompt the user to enter a positive integer
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    // Finding the last digit
+    last = n % 10;
+
+    while (n >= 10)
+    {
+        n = n / 10;
+    }
+
+    // Now n contains the first digit
+    first = n;
+
+    // Displaying the result
+    printf("First digit = %d\n", first);
+    printf("Last digit = %d\n", last);
+
+    return 0;
+}
