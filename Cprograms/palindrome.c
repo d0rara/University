76a99717a8a48613a9d73ca5e6c213955a351c79
d0rara <tribunejyotisaikia09421@gmail.com>
2026-02-02 02:20:04 +0530

    palindrome

---
+++ Cprograms/palindrome.c
@@ -0,0 +1,30 @@
+// Program to find palindrome
+
+#include <stdio.h>
+
+int main()
+{
+    int n, temp, rev = 0;
+
+    // Asking the user to enter a number
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // Store original number in temporary variable
+    temp = n;
+
+    // Loop to reverse the number
+    while (t != 0)
+    {
+        rev = (rev * 10) + (t % 10); // build reversed number
+        temp = temp / 10;                  // remove last digit
+    }
+
+    // Checking whether number is palindrome
+    if (rev == n)
+        printf("The number is a Palindrome.\n");
+    else
+        printf("The number is NOT a Palindrome.\n");
+
+    return 0;
+}
